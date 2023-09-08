7dc776a72b128c97c7228a2c2b6afcb0e6c094d8
rslepfso <rslepfso@gmail.com>
2023-09-08 20:10:22 +0300

    finish exercises 2.1-2.5

---
+++ part2/course-info/src/components/Total.jsx
@@ -0,0 +1,10 @@
+const Total = ({ parts }) => {
+  return (
+    <p>
+      Number of exercises{" "}
+      {parts.reduce((total, part) => total + part.exercises, 0)}
+    </p>
+  );
+};
+
+export default Total;
